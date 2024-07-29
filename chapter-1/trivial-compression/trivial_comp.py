f46cb7f9be04b1446801c08361355a491b14b975
Ivan Ramos Biagioni <140509979+ivan-maze@users.noreply.github.com>
2024-07-29 14:59:18 -0300

    method _decompress

--- chapter-1/trivial-compression/trivial_comp.py
+++ chapter-1/trivial-compression/trivial_comp.py
@@ -16,3 +16,22 @@ def _compress(self, gene:str) -> None:
             self.bit_string |= 0b11
         else:
             raise ValueError("Invalid Nucleotide:{}".format(nucleotide))
+
+def decompress(self) -> str:
+    gene: str = ""
+    for i in range(0, self.bit_string.bit_lenght()-1, 2): # -1 to exclude sentinel
+        bits: int = self.bit_string >> i & 0b11 # get just 2 relevant bits
+        if bits == 0b00: # A
+            gene += "A"
+        elif bits == 0b01: # C
+            gene += "C"
+        elif bits == 0b10: # G
+            gene += "G"
+        elif bits == 0b11: # T
+            gene += "T"
+        else:
+            raise ValueError("Invalid bits:{}".format(bits))
+    return gene[::-1] # invert string
+
+def _str_(self) -> str: # string representation
+    return self.decompress()
